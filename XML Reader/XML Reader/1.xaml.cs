3415523dff0a94bd2ee936dd780650d051c5ce5d
riccaforte <riccaforte@live.com>
2017-05-22 15:58:47 -0400

    XML Reader tool - will open a MISMO XML file and display XML using VS browser control; displays embedded PDF (base-64) using Acrobat PDFLib; extracts embedded PDF to file. This tool was meant to be able to display the XML data in an appraisal form, but I didn't get have time to finish it.

---
+++ XML Reader/XML Reader/1.xaml.cs
@@ -0,0 +1,17 @@
+﻿using System.Windows;
+using System.Windows.Input;
+using System.Windows.Media;
+
+namespace XML_Reader
+{
+    /// <summary>
+    /// Interaction logic for _1.xaml
+    /// </summary>
+    public partial class _1 : Window
+    {
+        public _1()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ XML Reader/XML Reader/MainWindow.xaml.cs
@@ -0,0 +1,21 @@
+﻿using System.Windows;
+
+namespace XML_Reader
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void button_Click(object sender, RoutedEventArgs e)
+        {
+            _1 page1 = new XML_Reader._1();
+            page1.Show();
+        }
+    }
+}
