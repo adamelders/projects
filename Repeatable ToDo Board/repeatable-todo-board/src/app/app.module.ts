3b3119d1aeda8690187a6968635a795c5ce13b2b
Adam Elders <Ramor.1990@gmail.com>
2019-11-20 04:54:32 -0500

    Sort of have a layout now

--- Repeatable ToDo Board/repeatable-todo-board/src/app/app.module.ts
+++ Repeatable ToDo Board/repeatable-todo-board/src/app/app.module.ts
@@ -4,9 +4,10 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
+import { ColumnComponent } from './column/column.component';
 
 @NgModule({
-  declarations: [AppComponent, HeaderComponent],
+  declarations: [AppComponent, HeaderComponent, ColumnComponent],
   imports: [BrowserModule, AppRoutingModule],
   providers: [],
   bootstrap: [AppComponent]
